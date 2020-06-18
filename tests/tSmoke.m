0a4cde8b6209bd2f68b174c2577b42587cc82f1e
gituser34 <66557066+gituser34@users.noreply.github.com>
2020-06-18 16:30:24 +0100

    remove failing test

--- tests/tSmoke.m
+++ tests/tSmoke.m
@@ -3,9 +3,5 @@ classdef tSmoke < matlab.unittest.TestCase
         function passTest(testcase)
             testcase.verifyTrue(true);
         end
-        
-        function failTest(testcase)
-            testcase.verifyFail();
-        end
     end
 end
